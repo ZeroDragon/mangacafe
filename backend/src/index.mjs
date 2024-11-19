0026c134ec72cfcb49ead622fac559e0f2583f1a
Zero Dragon <carlos@floresbenavides.com>
2024-11-18 22:19:30 -0600

    Update index.mjs

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -89,6 +89,11 @@ app.post('/api/sync', [verifyToken, getUser], async (req, res) => {
   res.json({ success: true, token: res.newToken })
 })
 
+app.get('/api/kill', (_req, res) => {
+  res.send('Killing in the name of')
+  throw new Error('Killing in the name of')
+})
+
 app.get('/api/', (_req, res) => {
   res.json({ message: 'Hello world' })
 })
